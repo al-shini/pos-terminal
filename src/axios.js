ac7d816ca0a47d4fa42c346fcba3bda517cdd1af
mnairat-shini <mohammad.n@shini.ps>
2022-06-23 14:22:25 +0200

    2

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://192.168.10.200:8080'
-    baseURL: 'http://127.0.0.1:8080'
+    baseURL: 'http://172.60.2.111:8080'
 
 });
 
